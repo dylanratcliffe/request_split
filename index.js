c9da95d5de9eb4969687f71808f0e62ff55edfd0
Dylan Ratcliffe <dylan.ratcliffe@puppetlabs.com>
2016-01-25 13:55:06 +1100

    Changed the code to read from a config file

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var http    = require('http');
 var request = require("request");
+var fs      = require('fs');
+var path    = require('path');
+var YAML    = require('yamljs');
+
+var config = YAML.load('config.yaml');
+
 
 // Okay so this block defines what the webserver will do. Inside is a request
 // function that will basically do a request whenever the running webserver
@@ -9,24 +15,33 @@ var server = http.createServer(function(req, res) {
   // All of the code in this block is executed every time we get a request
   console.log("Got a request");
 
-  // This is the code that goes ahead and makes the second request to the
-  // secondary Puppet Servers, there will be as many of these blocks as there
-  // are slave Puppet masters. (This will come form config eventually)
-  //
-  // We are going to have to put a block in here that waits for all of the
-  // requests to come back and then checks that they are all good.
-  request({
-    baseUrl: "https://www.google.com",
-    url: req.url,
-    method: req.method,
-    timeout: 10000,
-    followRedirect: true,
-    maxRedirects: 10
-  }, function(error, response, body) {
-    // In here is what gets executed after we have done the request. There will
-    // need to be some error handling in here
-    console.log(body);
-  });
+  for (var master in config) {
+    console.log(master)
+    // This is the code that goes ahead and makes the second request to the
+    // secondary Puppet Servers, there will be as many of these blocks as there
+    // are slave Puppet masters. (This will come form config eventually)
+    //
+    // We are going to have to put a block in here that waits for all of the
+    // requests to come back and then checks that they are all good.
+    request({
+      baseUrl: "https://master.mel.puppetlabs.demo",
+      url: req.url,
+      method: req.method,
+      timeout: 10000,
+      followRedirect: true,
+      maxRedirects: 10,
+      agentOptions: {
+        cert: fs.readFileSync(path.resolve(__dirname, config[master]['certFile'])),
+        key: fs.readFileSync(path.resolve(__dirname, config[master]['keyFile'])),
+        ca: fs.readFileSync(path.resolve(__dirname, config[master]['caCert'])),
+      },
+    }, function(error, response, body) {
+      // In here is what gets executed after we have done the request. There will
+      // need to be some error handling in here
+      console.log(body);
+    });
+  }
+
 
   console.log("Done");
 });
